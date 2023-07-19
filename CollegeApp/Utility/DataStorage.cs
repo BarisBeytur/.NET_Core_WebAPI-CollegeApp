b3ceff5ba35053a36e8cf8b13dfc17116d902811
BarisBeytur <beyturbaris@gmail.com>
2023-07-19 16:03:55 +0300

    Added pdf creator.

---
+++ CollegeApp/Utility/DataStorage.cs
@@ -0,0 +1,25 @@
+﻿using CollegeApp.Models;
+
+namespace CollegeApp.Utility
+{
+    public class DataStorage
+    {
+
+
+        public static List<Student> GetAllStudents() =>
+            new List<Student>
+            {
+                new Student { StudentName = "Baris Beytur", Email = "beyturbaris1@gmail.com", Address = "Sancaktepe"},
+                new Student { StudentName = "Ogrenci 2", Email = "ogrenci2@gmail.com", Address = "Kadıkoy"},
+                new Student { StudentName = "Ogrenci 3", Email = "ogrenci3@gmail.com", Address = "Uskudar"},
+                new Student { StudentName = "Ogrenci 4", Email = "ogrenci4@gmail.com", Address = "Maltepe"},
+                new Student { StudentName = "Ogrenci 5", Email = "ogrenci5@gmail.com", Address = "Küçükçekmece"},
+                new Student { StudentName = "Ogrenci 6", Email = "ogrenci6@gmail.com", Address = "Bağcılar"},
+                new Student { StudentName = "Ogrenci 7", Email = "ogrenci7@gmail.com", Address = "Sultanbeyli"},
+
+            };
+
+
+
+    }
+}
---
+++ CollegeApp/Utility/TemplateGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+
+namespace CollegeApp.Utility
+{
+    public static class TemplateGenerator
+    {
+        public static string GetHTMLString()
+        {
+            var students = DataStorage.GetAllStudents();
+            var sb = new StringBuilder();
+            sb.Append(@"
+                        <html>
+                            <head>
+                            </head>
+                            <body>
+                                <div class='header'><h1>This is the generated PDF report!!!</h1></div>
+                                <table align='center'>
+                                    <tr>
+                                        <th>Student Name</th>
+                                        <th>Email</th>
+                                        <th>Address</th>                                    
+                                    </tr>");
+            foreach (var student in students)
+            {
+                sb.AppendFormat(@"<tr>
+                                    <td>{0}</td>
+                                    <td>{1}</td>
+                                    <td>{2}</td>                                   
+                                  </tr>", student.StudentName, student.Email, student.Address);
+            }
+            sb.Append(@"
+                                </table>
+                            </body>
+                        </html>");
+            return sb.ToString();
+        }
+
+
+    }
+}
